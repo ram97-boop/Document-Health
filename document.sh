606e8aab5a34997298cd57d029fde52e2be7a8d1
Ram Linux <ram97-boop@users.noreply.github.com>
2022-01-05 23:02:30 +0100

    Add hours of floor-sitting to be documented

--- document.sh
+++ document.sh
@@ -8,22 +8,28 @@ echo For how many hours have you sat on a chair?
 
 read chairHours
 
+echo How many hours sat on the floor?
+
+read floorHours
+
 touch data.txt &&
 # If the last update was today
-if [ "$(echo $(tail -n 4 data.txt | head -n 1 | cut -d " " -f 1-4))" == "$(echo $(date) | cut -d " " -f 1-4)" ]
+if [ "$(echo $(tail -n 5 data.txt | head -n 1 | cut -d " " -f 1-4))" == "$(echo $(date) | cut -d " " -f 1-4)" ]
 	then
-		head -n -3 data.txt > tmp.txt &&
+		head -n -5 data.txt > tmp.txt &&
 		cat tmp.txt > data.txt &&
 		rm tmp.txt &&
 		echo $(date) >> data.txt &&
 		echo "Glasses of water: $glassesOfWater" >> data.txt &&
 		echo "Hours on a chair: $chairHours" >> data.txt &&
+		echo "Hours on floor: $floorHours" >> data.txt &&
 		echo "" >> data.txt
 
 else
 	echo $(date) >> data.txt &&
 	echo "Glasses of water: $glassesOfWater" >> data.txt &&
 	echo "Hours on a chair: $chairHours" >> data.txt &&
+	echo "Hours on floor: $floorHours" >> data.txt &&
 	echo "" >> data.txt
 
 fi
